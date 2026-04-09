3a7bd2349036a726cb3f7852ea77031fb78cae2c
ByMoyan <aa123ben@gmail.com>
2026-04-09 15:45:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)
+    browser = p.chromium.launch(
+        headless=True,
+        executable_path="/usr/bin/chromium"
+    )
     page = browser.new_page()
     page.goto("https://aternos.org/go/", wait_until="domcontentloaded")
     print("页面标题:", page.title())
