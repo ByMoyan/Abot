eb602aa007ce3a29159b563a31aa5505ea8d34db
ByMoyan <aa123ben@gmail.com>
2026-04-09 22:58:14 +0800

    我要伪装 part III

--- main.py
+++ main.py
@@ -103,6 +103,20 @@ def run_playwright():
             Object.defineProperty(navigator, 'plugins', {
                 get: () => [1, 2, 3, 4, 5]
             });
+
+            Object.defineProperty(navigator, 'maxTouchPoints', {
+                get: () => 1
+            });
+
+            Object.defineProperty(navigator, 'languages', {
+                get: () => ['zh-TW']
+            });
+
+            const originalQuery = window.navigator.permissions.query;
+            window.navigator.permissions.query = (parameters) =>
+                parameters.name === 'notifications'
+                    ? Promise.resolve({ state: Notification.permission })
+                    : originalQuery(parameters);
         """)
 
         page.goto("https://aternos.org/go/", wait_until="domcontentloaded")
