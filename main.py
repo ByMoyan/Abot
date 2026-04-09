20ed3bd7594974a26f5a995584a1892ce64b9cb9
ByMoyan <aa123ben@gmail.com>
2026-04-09 16:02:38 +0800

    越过cloudflare

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
     browser = p.chromium.launch(
         headless=True,
-        executable_path="/usr/bin/chromium"
+        executable_path="/usr/bin/chromium",
+        args=["--disable-blink-features=AutomationControlled"]
     )
     page = browser.new_page()
+    page.add_init_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     page.goto("https://aternos.org/go/", wait_until="domcontentloaded")
     print("页面标题:", page.title())
     browser.close()
