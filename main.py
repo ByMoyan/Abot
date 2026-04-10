9c1d0f6455d4fa3618f9a5ed64b2fe478ad9c0c5
ByMoyan <aa123ben@gmail.com>
2026-04-10 12:25:13 +0800

    A主页测试

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def run_playwright():
         )
         page = browser.new_page()
         page.add_init_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-        page.goto("https://youtube.com", wait_until="domcontentloaded")
+        page.goto("https://aternos.org/:zh-TW/", wait_until="domcontentloaded")
 
         current_url = page.url
         current_title = page.title()
